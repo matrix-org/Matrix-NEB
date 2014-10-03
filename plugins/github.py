b1ec2f835af27e0dc18a20d854a0a61a37a328c2
Kegan Dougal <kegan@matrix.org>
2014-10-03 15:12:31 +0100

    Added support for deleted branches.

--- plugins/github.py
+++ plugins/github.py
@@ -84,18 +84,28 @@ class GithubPlugin(Plugin):
             projects.append(info["repo"])
             self.store.set("known_projects", projects)
 
+        push_message = ""
 
-
-        # form the template:
-        # [<repo>] <username> pushed <num> commits to <branch>: <git.io link>
-        # 1<=3 of <branch name> <short hash> <full username>: <comment>
-        push_message = "[%s] %s pushed to %s: %s  - %s" % (
-            info["repo"],
-            info["commit_username"],
-            info["branch"],
-            info["commit_msg"],
-            info["commit_link"]
-        )
+        if info["type"] == "delete":
+            push_message = "[%s] %s deleted %s" % (
+                info["repo"],
+                info["commit_username"],
+                info["branch"]
+            )
+        elif info["type"] == "commit":
+            # form the template:
+            # [<repo>] <username> pushed <num> commits to <branch>: <git.io link>
+            # 1<=3 of <branch name> <short hash> <full username>: <comment>
+            push_message = "[%s] %s pushed to %s: %s  - %s" % (
+                info["repo"],
+                info["commit_username"],
+                info["branch"],
+                info["commit_msg"],
+                info["commit_link"]
+            )
+        else:
+            log.warn("Unknown push type. %s", info["type"])
+            return
 
         # send messages to all rooms registered with this project.
         for (room_id, room_info) in self.state.iteritems():
@@ -182,20 +192,30 @@ class GithubPlugin(Plugin):
         repo_name = j["repository"]["full_name"]
         # strip 'refs/heads' from 'refs/heads/branch_name'
         branch = '/'.join(j["ref"].split('/')[2:])
-        commit_msg = j["head_commit"]["message"]
-        commit_name = j["head_commit"]["committer"]["name"]
 
+        commit_msg = ""
+        commit_name = ""
+        commit_link = ""
+        push_type = "commit"
+
+        if j["head_commit"]:
+            commit_msg = j["head_commit"]["message"]
+            commit_name = j["head_commit"]["committer"]["name"]
+            commit_link = j["head_commit"]["url"]
+            # short hash please
+            short_hash = commit_link.split('/')[-1][0:8]
+            commit_link = '/'.join(commit_link.split('/')[0:-1]) + "/" + short_hash
+        elif j["deleted"]:
+            # looks like this branch was deleted, no commit and deleted=true
+            commit_name = j["pusher"]["name"]
+            push_type = "delete"
+                
         commit_uname = None
         try:
             commit_uname = j["head_commit"]["committer"]["username"]
         except KeyError:
             # possible if they haven't tied up with a github account
             commit_uname = commit_name
-
-        commit_link = j["head_commit"]["url"]
-        # short hash please
-        short_hash = commit_link.split('/')[-1][0:8]
-        commit_link = '/'.join(commit_link.split('/')[0:-1]) + "/" + short_hash
 
         self.on_receive_github_push({
             "branch": branch,
@@ -204,7 +224,8 @@ class GithubPlugin(Plugin):
             "commit_username": commit_uname,
             "commit_name": commit_name,
             "commit_link": commit_link,
-            "commit_hash": short_hash
+            "commit_hash": short_hash,
+            "type": push_type
         })
 
     def sync(self, matrix, sync):
